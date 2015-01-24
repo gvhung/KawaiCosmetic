94030b08026332063e34fc36ede507cc4cc6af7f
Nikita <unilogx@gmail.com>
2015-01-24 23:44:25 +0400

    Moved catalog html code to resource file

---
+++ Pages/Resources/CatalogMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RussianKawaiShop.Pages.Resources
+{
+    class CatalogMenu : RussianKawaiShop
+    {
+        public override string TemplateAddr
+        {
+            get { return "Resources.CatalogMenu.html"; }
+        }
+    }
+}
