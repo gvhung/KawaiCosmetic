698508ccfdc97b15c2bba07cce2ce6c123db7642
Nikita <unilogx@gmail.com>
2015-03-10 16:07:37 +0300

    Added ajax for adding product to cart

--- WebSocket.cs
+++ WebSocket.cs
@@ -36,18 +36,39 @@ namespace RussianKawaiShop
 
                 if (Action == "AddProductToCartAction")
                 {
-                    int productId, num, productColorId;
-
-                    if (int.TryParse(WSData[1], out productId) && int.TryParse(WSData[2], out num) && int.TryParse(WSData[1], out productId) && int.TryParse(WSData[3], out productColorId))
+                    if (this.AddToCart(WSData[1], WSData[2], WSData[3], client))
                     {
-                        cartService.AddProduct(productId, num, cartService.GetCookie(client), productColorId);
                         client.SendWebsocket("CountItemsInCartAction" + BaseFuncs.WSplit + cartService.CountProductsNum(cartService.GetCookie(client)));
+                    }
+                }
+            }
+            else if(client.PostParam("action") != null)
+            {
+                string Action = client.PostParam("action");
+                if(Action == "AddProductToCartAction")
+                {
+                    if (this.AddToCart(client.PostParam("id"), client.PostParam("num"), client.PostParam("productColor"), client))
+                    {
+                        client.HttpSend(cartService.CountProductsNum(cartService.GetCookie(client)).ToString());
+                        return false;
                     }
                 }
             }
 
             client.HttpSend(TemplateActivator.Activate(this, client));
-            return true;
+            return false;
+        }
+
+        private bool AddToCart(string productID, string num, string productColorId, Client client)
+        {
+            int _productId, _num, _productColorId;
+
+            if (int.TryParse(productID, out _productId) && int.TryParse(num, out _num) && int.TryParse(productColorId, out _productColorId))
+            {
+                cartService.AddProduct(_productId, _num, cartService.GetCookie(client), _productColorId);
+                return true;
+            }
+            return false;
         }
     }
 }
