c602010ed48ff35b9a84178e03732c0cb6c3ebe6
Nikita <unilogx@gmail.com>
2015-04-09 13:26:25 +0700

    Added logout page for partner

---
+++ RussianKawaiPartner/Pages/LogoutPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpServer;
+
+namespace RussianKawaiPartner
+{
+    class LogoutPage : RussianKawaiPartner
+    {
+        public override PageType PageType
+        {
+            get { return PageType.Once; }
+        }
+        public override string URL
+        {
+            get { return "/logout/"; }
+        }
+        public override ushort AccessLevel
+        {
+            get { return 1; }
+        }
+        public override bool Init(Client client)
+        {
+            client.SetCookie["PartnerSession"] = null;
+            client.Redirect("/");
+            return false;
+        }
+    }
+}
